331129bd7d561c699d3d085e63bfdbd6c027e9bc
len23 <lenin.montalvo.23@hotmail.com>
2020-05-26 16:37:02 -0500

    adding comment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-
+//
 const app = express();
 
 //Define paths for Express config
